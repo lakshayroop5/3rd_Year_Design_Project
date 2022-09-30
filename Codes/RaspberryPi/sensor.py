b6730268662da23a21bfa1d8a7739266c1c01e15
Prat2030 <pratyushjindal2003@gmail.com>
2022-09-30 21:10:55 +0530

    insert sensor to mongoDB database

--- Codes/RaspberryPi/sensor.py
+++ Codes/RaspberryPi/sensor.py
@@ -4,6 +4,8 @@ import datetime
 import glob
 # import MySQLdb // for MySQL database
 import Adafruit_DHT
+import pymongo
+from pymongo import MongoClient
 import RPi.GPIO as GPIO
 from time import strftime
 
@@ -12,8 +14,13 @@ GPIO.setup(8, GPIO.IN)    #8 is the pin number for LDR
 GPIO.setup(10, GPIO.OUT)  #10 is the pin number for LED
 
 #Variables for MySQL
-db = MySQLdb.connect(host="localhost", user="admin", passwd="pass123", db="sensor") # replace password with your password
-cur = db.cursor()
+# db = MySQLdb.connect(host="localhost", user="admin", passwd="pass123", db="sensor") # replace password with your password
+# cur = db.cursor()
+
+#Variables for MongoDB
+client = MongoClient("mongodb+srv://admin:li8TXxeO8n416O8t@cluster0.h0ymi0w.mongodb.net/?retryWrites=true&w=majority")
+db = client['sensor']
+collection = db['sensorData']
 
 sensor=Adafruit_DHT.DHT11
 sensor_pin = 4
@@ -41,10 +48,16 @@ def ldrRead(): #read LDR, return 0 or 1
                 return True
 
 
+
 secs = dateTime()
 temperatures = tempRead()
 humiditys = humidityRead()
 lighting = ldrRead()
+
+# Write data to MongoDB database
+def mongoWrite(): 
+        data = {"time": dateTime(), "temperature": tempRead(), "humidity": humidityRead(), "ldr": ldrRead()}
+        collection.insert_one(data)
 
 # write data to MySQL database
 # sql = ("""INSERT INTO dhtsensor (datetime,temperature,humidity) VALUES (%s,%s,%s)""", (secs, temperatures, humiditys))
